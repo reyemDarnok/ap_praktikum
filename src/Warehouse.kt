d1bbd1edb9c07c8af3af7985084ebd86d819ca2e
Ben Konrad Meyer <ben.k.meyer@gmail.com>
2020-05-15 11:46:43 +0200

    recovered fillWarehouse

--- src/Warehouse.kt
+++ src/Warehouse.kt
@@ -33,15 +33,12 @@ class Warehouse {
     fun fillWarehouse(productName: String, basePrice: Double, productDescription: String,
                       chargeOnTop: Double = 50.0, initialStockUnits: Int = 3) {
         val product = Product(productName, basePrice, basePrice * (1 + chargeOnTop / 100), productDescription)
-        val discountProduct = DiscountProduct(productName + "Discounted", basePrice, basePrice * (1 + chargeOnTop / 100), productDescription, DiscountType.SummerSale)
         for (i in 0..initialStockUnits) {
             product.addStock(StockUnit(Random.nextInt(0, 20), Random.nextInt(0, 20)))
-            discountProduct.addStock(StockUnit(Random.nextInt(0, 20), Random.nextInt(0, 20)))
         }
         product.addReview(PlainReview(Random.nextInt()))
         product.addReview(LimitedReview(Random.nextDouble(), String(Random.nextBytes(Random.nextInt(0, 255)))))
         product.addReview(SmartReview(Random.nextInt(0, 5)))
-        products.add(discountProduct)
         products.add(product)
     }
 }
