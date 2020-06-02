13958015d82e2895a5520123794294ebd8954334
Ben Konrad Meyer <ben.k.meyer@gmail.com>
2020-06-02 17:58:40 +0200

    switched to null safe types in mergesort

--- src/order/OrderProcessing.kt
+++ src/order/OrderProcessing.kt
@@ -169,8 +169,8 @@ class OrderProcessing : MutableIterable<Order> {
         var fast = head
 
         while (fast.next != null && fast.next?.next != null) {
-            slow = slow.next
-            fast = fast.next?.next
+            slow = slow.next!!
+            fast = fast.next!!.next!!
         }
 
         return slow
