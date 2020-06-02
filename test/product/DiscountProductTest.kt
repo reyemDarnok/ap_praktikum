2d363506e761306d5754b9e4f29a52b82346725b
Ben Konrad Meyer <ben.k.meyer@gmail.com>
2020-06-02 13:14:17 +0200

    added tests for DiscountProduct

---
+++ test/product/DiscountProductTest.kt
@@ -0,0 +1,12 @@
+package product
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class DiscountProductTest {
+    @Test
+    fun `salesPrice is adjusted`() {
+        val discountProduct = DiscountProduct("", 3.0, 10.0, "", DiscountType.SellEverything)
+        assertEquals(5.0, discountProduct.salesPrice)
+    }
+}
