f2dbd9349c309a87fc209eb9c167be26ba42ecc3
Ben Konrad Meyer <ben.k.meyer@gmail.com>
2020-06-03 11:13:45 +0200

    moved to apply in product tests

--- test/product/ProductTest.kt
+++ test/product/ProductTest.kt
@@ -1,5 +1,6 @@
 package product
 
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import review.PlainReview
@@ -11,10 +12,16 @@ class ProductTest {
 
     @Before
     fun init() {
-        Product.longestAmount = 0
-        Product.longestPrice = 0
-        Product.longestName = 0
         product = Product.create("name", 5.0, 10.0, "desc")
+    }
+
+    @After
+    fun cleanup() {
+        Product.apply {
+            longestAmount = 0
+            longestName = 0
+            longestPrice = 0
+        }
     }
 
     @Test
@@ -31,27 +38,35 @@ class ProductTest {
 
     @Test
     fun `valueOfAllItems works`() {
-        product.addStock(StockUnit(10, 5))
-        assertEquals(50.0, product.valueOfAllItems)
+        product.apply {
+            addStock(StockUnit(10, 5))
+            assertEquals(50.0, valueOfAllItems)
+        }
     }
 
     @Test
     fun `salesValueOfAllItems works`() {
-        product.addStock(StockUnit(10, 5))
-        assertEquals(100.0, product.salesValueOfAllItems)
+        product.apply {
+            addStock(StockUnit(10, 5))
+            assertEquals(100.0, salesValueOfAllItems)
+        }
     }
 
     @Test
     fun `availableItems works`() {
-        product.addStock(StockUnit(10, 5))
-        assertEquals(10, product.availableItems)
+        product.apply {
+            addStock(StockUnit(10, 5))
+            assertEquals(10, availableItems)
+        }
     }
 
     @Test
     fun `multi stock availableItems works`() {
-        product.addStock(StockUnit(10, 5))
-        product.addStock(StockUnit(10, 2))
-        assertEquals(20, product.availableItems)
+        product.apply {
+            addStock(StockUnit(10, 5))
+            addStock(StockUnit(10, 2))
+            assertEquals(20, availableItems)
+        }
     }
 
     @Test
@@ -74,47 +89,55 @@ class ProductTest {
 
     @Test
     fun `addStock adds Stock`() {
-        product.addStock(StockUnit(10, 5))
-        assertEquals(10, product.availableItems)
-        product.addStock(StockUnit(10, 5))
-        assertEquals(20, product.availableItems)
-        assertEquals(2, Product.longestAmount)
+        product.apply {
+            addStock(StockUnit(10, 5))
+            addStock(StockUnit(10, 5))
+            assertEquals(20, availableItems)
+            assertEquals(2, Product.longestAmount)
+        }
     }
 
     @Test
     fun `addReview works`() {
-        product.addReview(PlainReview(5))
-        assertEquals(1, product.reviews.size)
+        product.apply {
+            addReview(PlainReview(5))
+            assertEquals(1, reviews.size)
+        }
     }
 
     @Test
     fun `cleanStock removes expired and empty`() {
         val toRemain = StockUnit(5, 5)
-        product.addStock(toRemain)
-        product.addStock(StockUnit(10, -5))
-        product.addStock(StockUnit(0, 10))
-        product.addStock(StockUnit(-5, 10))
-        product.addStock(StockUnit(0, 0))
-        product.cleanStock()
-        assertEquals(1, product.stockUnits.size)
-        assertEquals(toRemain, product.stockUnits[0])
+        product.apply {
+            addStock(toRemain)
+            addStock(StockUnit(10, -5))
+            addStock(StockUnit(0, 10))
+            addStock(StockUnit(-5, 10))
+            addStock(StockUnit(0, 0))
+            cleanStock()
+            assertEquals(1, stockUnits.size)
+            assertEquals(toRemain, stockUnits[0])
+        }
     }
 
     @Test
     fun `takeItems from one stock`() {
-        product.addStock(StockUnit(10, 3))
-        assertEquals(5, product.takeItems(5))
-        assertEquals(5, product.availableItems)
+        product.apply {
+            addStock(StockUnit(10, 3))
+            assertEquals(5, takeItems(5))
+            assertEquals(5, availableItems)
+        }
     }
 
     @Test
     fun `takeItems from multiple stocks`() {
         val toRemain = StockUnit(10, 100000)
-        product.addStock(toRemain)
-        product.addStock(StockUnit(2, 2))
-        assertEquals(5, product.takeItems(5))
-        assertTrue(product.stockUnits.contains(toRemain))
-        assertEquals(7, toRemain.quantity)
+        product.apply {
+            addStock(toRemain)
+            addStock(StockUnit(2, 2))
+            assertEquals(5, takeItems(5))
+            assertTrue(stockUnits.contains(toRemain))
+            assertEquals(7, toRemain.quantity)
+        }
     }
-
 }
